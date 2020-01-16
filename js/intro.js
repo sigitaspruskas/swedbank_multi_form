96e290f6cf61e24bc8d27e4bf07d27ae9e16fae3
Sigitas <sigitaspruskas@gmail.com>
2020-01-16 15:30:28 +0200

    v2: working app on mobile design, added form success window

--- js/intro.js
+++ js/intro.js
@@ -1,61 +1,109 @@
-const introBtn = document.querySelector('.intro-btn');
 const introContainer = document.querySelector('.intro')
 
 let currentStepId = 0;
 let stepToBeLoaded = currentStepId;
-let fieldsetArray = document.querySelectorAll('fieldset');
+let fieldsetNodeList = document.querySelectorAll('fieldset');
 const currentStepContainer = document.getElementsByTagName("fieldset");
 
+/**
+ * Function shows range input value in HTML element.
+ * @param {*} val 
+ */
 function showInputValue(val) {
     return document.querySelector('#sumValue').innerHTML = val + ' eur';
 }
 
-const leaveIntro = () => {
+/**
+ * Function hides intro window, and renders first form step.
+ */
+const goToForm = () => {
     event.preventDefault();
-    introContainer.classList.add('slide-out');
-    loadStep();
+    introContainer.className = 'intro slide-out';
+    loadNextStep();
     setTimeout(() => introContainer.classList.add('hide'), 200)
 }
 
-function validateInputs(value) {
-    console.log(event);
+/**
+ * Function closes form step 1 question, and renders Intro container.
+ */
+function closeForm() {
     event.preventDefault();
-    // console.log(document.getElementById('moneySum').value);
-    loadStep();
+    currentStepContainer[0].className = 'hide';
+    introContainer.className = 'intro slide-in';
+    currentStepId = 0;
+    stepToBeLoaded = currentStepId;
+};
+
+/**
+ * Function handles which question content to load in HTML, depending on previous answer.
+ */
+function renderOptionalStep() {
+    if (currentStepId === 2) {
+        const selection = document.querySelector('#isEmployed');
+        const value = selection.value;
+        const questionContainer = document.querySelector('#dependingOnAnswer');
+
+        if (value === 'yes') {
+            // Question about time spent at current job position will be rendered
+            questionContainer.innerHTML = `
+            <h1>Vartojimo Paskolos paraiška</h1>
+            <h2>Dabartinė darbovietė</h2>                
+            <p class="text-justify">
+                Kiek laiko dirbate dabartinėje darbovietėje?
+            </p>
+            <select id="jobStatus">
+                <option value="under6Months">Mažiau nei 6 mėn.</option>
+                <option value="underOneYear">Tarp 6 mėn ir 1 metų</option>
+                <option value="moreThanAYear">Daugiau nei 1 metai</option>
+            </select>
+            <input type="submit" value="Kitas" class="button" onclick="nextBtn()">
+            <input type="submit" value="Grįžti" class="button" onclick="backBtn()">`;
+        } else {
+            // Question about marital status will be rendered
+            questionContainer.innerHTML = `
+            <h1>Vartojimo Paskolos paraiška</h1>
+            <h2>Ar turite sutuoktinį?</h2>                
+            <p class="text-justify">
+                Nurodykite šeiminynį statusą.
+            </p>
+            <select id="isMarried">
+                <option value="yes">Taip</option>
+                <option value="no">Ne</option>
+            </select>
+            <input type="submit" value="Kitas" class="button" onclick="nextBtn()">
+            <input type="submit" value="Grįžti" class="button" onclick="backBtn()">`;
+        }
+    }
 }
 
-// Valdiate loop ang go next
+/**
+ * Basic inputs validation. If validation is successful, function handles current step counter 
+ * variables, then calls renderOptionalStep() and loadNextStep() functions.
+ */
 function nextBtn() {
-    console.log(fieldsetArray.length);
     event.preventDefault();
     let validation = true;
-    let fieldset = document.querySelectorAll('fieldset')[currentStepId];
+    let fieldset = fieldsetNodeList[currentStepId];
     let inputsCount = fieldset.querySelectorAll('input').length;
     let textarea = fieldset.querySelector('textarea');
-    let select = fieldset.querySelector('select');
 
-    // validating inputs
+    // Inputs validation
     for (i = 0; i < inputsCount; ++i) {
         var inputEl = fieldset.querySelectorAll('input')[i];
 
         if (inputEl.getAttribute('type') !== 'submit' && inputEl.getAttribute('type') !== 'range') {
-            console.log(inputEl);
-            console.log(i);
+
             if (inputEl.value === '') {
                 validation = false;
                 let errMsg = fieldset.querySelector('#err-msg');
                 errMsg.innerHTML = 'Neužpildėte visų laukų!';
                 errMsg.className = 'error';
-                console.log('emptyyyyy');
                 break;
-            } else {
-                // errMsg.className = 'hide';
-                validation = true;
-            }
+            } else validation = true;
         }
     }
 
-    // validate textarea value
+    // Textarea validation
     if (textarea) {
         let errMsg = document.getElementById('err-msg');
 
@@ -64,85 +112,103 @@ function nextBtn() {
             errMsg.className = 'error';
             validation = false;
         }
-        else {
-            errMsg ? errMsg.className = 'hide': null;
-        }
+        else errMsg ? errMsg.className = 'hide' : null;
+
     }
 
+    // validation success 
     if (validation) {
         if (event.target.value === 'Siųsti') {
-            // Send form data
+            // Do smth to send form data
+            const div = document.createElement('div')
+            div.innerHTML = `
+            <h2>Užklausa sėkmingai išsiųsta!</h2>                
+            <p class="text-justify">
+                Gavome jūsų prašymą, mūsų konsultantai peržvelgs jį, ir susisieks su jumis per 3 darbo dienas.
+            </p>`;
+            currentStepContainer[currentStepId].className = 'hide';
+            setTimeout(() => {
+                div.className = 'slide-in';
+                document.querySelector('.container').appendChild(div);
+            }, 10);
         } else {
-            loadStep()
-            console.log('validation succes call next()');
+            renderOptionalStep();
+            loadNextStep();
+            currentStepId++;
+            stepToBeLoaded++;
         }
     }
 }
 
-function loadStep() {
-    event ? event.preventDefault() : null;
-    currentStepId === 4 ? summaryText() : null;
+/**
+ * Function handles previous step rendering to HTML
+ */
+function backBtn() {
+    event.preventDefault();
+    stepToBeLoaded = currentStepId - 1;
+    currentStepContainer[currentStepId].className = 'hide';
+    setTimeout(() => {
+        currentStepContainer[stepToBeLoaded].className = 'slide-in';
+        currentStepId--;
+        stepToBeLoaded = currentStepId + 1;
+    }, 10);
+}
 
-    if (event.target.innerHTML === 'Pildyti paraišką') {
-        // LOADS STEP 0
-        // console.log('currentStepId: ' + currentStepId);
+/**
+ * Function renders next question to HTML by adding and removing css classes.
+ */
+function loadNextStep() {
+    event ? event.preventDefault() : null;
+    currentStepId === 3 ? summaryText() : null;
+
+    if (event.target.value !== 'Kitas') {
+        // This activates when Intro window button is clicked
         currentStepContainer[stepToBeLoaded].className = 'slide-in';
         stepToBeLoaded++;
-        // console.log('stepToBeLoaded: ' + stepToBeLoaded);
-    } else if (event.target.value === 'Kitas' || event.target.value === 'Kitas') {
-        // Hiding current form input container
+    } else {
         currentStepContainer[currentStepId].className = 'slide-out';
-        // Enabling newly loaded container
         currentStepContainer[stepToBeLoaded].className = 'slide-in';
         // setting timeOut for events that should happen after animations.
         setTimeout(() => {
-            currentStepContainer[currentStepId].className = 'hide';
-            currentStepId++;
-            stepToBeLoaded = currentStepId + 1
-            // console.log('currentStepId: ' + currentStepId);
-            // console.log('stepToBeLoaded: ' + stepToBeLoaded);
+            currentStepContainer[currentStepId - 1].className = 'hide';
         }, 200);
     }
-    else if (event.target.value === 'Grįžti') {
-        stepToBeLoaded = currentStepId - 1;
-        // Hiding current form input container
-        currentStepContainer[currentStepId].className = 'slide-out';
-        // setting timeOut for events that should happen after animations.
-        setTimeout(() => {
-            // Enabling newly loaded container
-            currentStepContainer[stepToBeLoaded].className = 'slide-in';
-            currentStepContainer[currentStepId].className = 'hide';
-            currentStepId--;
-            stepToBeLoaded = currentStepId + 1;
-            // console.log('currentStepId: ' + currentStepId);
-            // console.log('stepToBeLoaded: ' + stepToBeLoaded);
-        }, 200);
-    }
-
 };
 
+/**
+ * Text handling for summary and contacts step.
+ */
 function summaryText() {
-    let money = document.querySelector('#money');
-    let text = document.querySelector('#whatFor');
-    let employ = document.querySelector('#employed');
-    let married = document.querySelector('#married');
-    let job = document.querySelector('#job');
+    // selected HTML elements for summary information placement
+    const money = document.querySelector('#money'),
+        text = document.querySelector('#whatFor'),
+        employ = document.querySelector('#employed'),
+        married = document.querySelector('#married'),
+        job = document.querySelector('#job');
 
     // Text from Step 3 
-    let isEmployedSel = document.querySelector('#isEmployed');
-    let isEmployedText = isEmployedSel.options[isEmployedSel.selectedIndex];
+    const isEmployedSel = document.querySelector('#isEmployed');
+    const isEmployedText = isEmployedSel.options[isEmployedSel.selectedIndex];
+
     // Text from Step 4
-    let isMarriedSel = document.querySelector('#isMarried');
-    let isMarriedText = isMarriedSel.options[isMarriedSel.selectedIndex];
+    const isMarriedSel = document.querySelector('#isMarried');
+    if (isMarriedSel) {
+        const isMarriedText = isMarriedSel.options[isMarriedSel.selectedIndex];
+        married.innerHTML = `${isMarriedText.text}`;
+        document.querySelector('#married-text-box').className = 'summary-text-box';
+        document.querySelector('#job-text-box').className = 'hide';
+    };
+
     // Text from Step 5 text
-    let howLongSel = document.querySelector('#jobStatus');
-    let howLongText = howLongSel.options[howLongSel.selectedIndex];
+    const howLongSel = document.querySelector('#jobStatus');
+    if (howLongSel) {
+        const howLongText = howLongSel.options[howLongSel.selectedIndex];
+        job.innerHTML = `${howLongText.text}`;
+        document.querySelector('#married-text-box').className = 'hide';
+        document.querySelector('#job-text-box').className = 'summary-text-box';
+    };
 
     money.innerHTML = `${document.querySelector('#moneySum').value} eur.`;
     text.innerHTML = `${document.querySelector('#text').value}`;
     employ.innerHTML = `${isEmployedText.text}`;
-    married.innerHTML = `${isMarriedText.text}`;
-    job.innerHTML = `${howLongText.text}`;
-}
-
-introBtn.addEventListener('click', leaveIntro);
+};
